07d8fe5ee46ce3da9e1d4b00598e2f2db2b6d0cd
Yadhies1989 <yadi.sepriadi@gmail.com>
2022-06-04 22:11:48 +0700

    commit chalenge 5

--- index.js
+++ index.js
@@ -6,15 +6,32 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = function (param1, param2) {
+  let hasil = [];
+
+  for (let counter = 0 ; counter < param1.length; counter++) {
+      // hasil.push((counter + 1) + '.' + ' ' + param2(param1[counter]));
+      if (param2(param1[counter])){
+          hasil.push((counter + 1) + '.' + ' ' + param1[counter]);
+      }
+  }
+
+  return hasil;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = function () {
+  let hasil = names.sort();
+  return hasil ;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending =function () {
+  let hasil = names.sort().reverse();
+  return hasil ;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
